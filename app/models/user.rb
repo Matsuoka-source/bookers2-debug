05ac84095387571d4d8bf2dd5b73891cc0cb843f
Matsuoka-source <tm19930425@gmail.com>
2021-05-03 07:45:07 +0000

    comment and good

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,7 @@ class User < ApplicationRecord
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
 
+  has_many :favorites, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+
 end
