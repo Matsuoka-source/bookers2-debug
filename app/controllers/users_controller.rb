67686f65aa351d52aaf9f61f5d20daea72c4434c
Matsuoka-source <tm19930425@gmail.com>
2021-05-05 23:27:17 +0000

    follow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_correct_user, only: [:edit, :update]
+  before_action :set_user, only: %I[show edit update destroy followeds followers]
 
   def show
     @user = User.find(params[:id])
@@ -26,6 +27,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def  followeds
+    @followeds = @user.followed_users
+  end
+
+  def  followers
+    @followers = @user.follower_users
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
@@ -36,5 +45,9 @@ class UsersController < ApplicationController
     unless @user == current_user
       redirect_to user_path(current_user)
     end
+  end
+
+  def  set_user
+    @user = User.find(params[:id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,25 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :post_comments, dependent: :destroy
 
+ # 自分がフォローされる
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # 自分がフォローする
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # 被フォロー→自分をフォロー
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  # 与フォロー→自分がフォロー
+  has_many :followings, through: :relationships, source: :followed
+
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
+
 end
